6caae1bf707ba90056d4faff49fc5f4dba202af1
trongnhan19081994 <trongnhan19081994@gmail.com>
2017-11-27 15:30:25 +0700

    Project 02 QL Cong Viec

---
+++ project02-qlcongviec/src/components/TaskList.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import TaskItems from './TaskItems';
+
+class TaskList extends Component {
+  render() {
+    return (
+       
+        <table className="table table-bordered table-hover">
+            <thead>
+                <tr>
+                    <th className="text-center">STT</th>
+                    <th className="text-center">Tên</th>
+                    <th className="text-center">Trạng Thái</th>
+                    <th className="text-center">Hành Động</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td></td>
+                    <td>
+                        <input type="text" className="form-control" />
+                    </td>
+                    <td>
+                        <select className="form-control">
+                            <option value="-1">Tất Cả</option>
+                            <option value="0">Ẩn</option>
+                            <option value="1">Kích Hoạt</option>
+                        </select>
+                    </td>
+                    <td></td>
+                </tr>
+
+                <TaskItems />
+
+            </tbody>
+        </table>
+
+    );
+  }
+}
+
+export default TaskList;
